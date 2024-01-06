06440d60c96663c793c559bc85186a2835fa55c0
Evan Zhou <evanzhoudev@gmail.com>
2024-01-06 14:13:56 -0800

    ✨ Allow copying of answers

--- src/friendly.jsx
+++ src/friendly.jsx
@@ -1,5 +1,5 @@
 import useGemini from "./api/gemini";
 
 export default function Friendly(props) {
-  return useGemini(props, "Make the following text seem more friendly.");
+  return useGemini(props, "Make the following text seem more friendly. ONLY return the modified text and nothing else.", true);
 }
